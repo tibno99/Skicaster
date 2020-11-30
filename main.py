62bba9a4f094eb852895dbd79d8a8e824dba046b
Tobin Eberle <teberle@ualberta.ca>
2020-11-29 22:36:31 -0700

    Got dictionary working, next step push to DB

--- main.py
+++ main.py
@@ -3,70 +3,75 @@ import requests as rq
 from bs4 import BeautifulSoup as soup 
 import psycopg2 as postgres
 import re 
+import datetime
 
 
 
 def main():
-        #Establish connection to database
-        day = datetime.date
-        try:
-            conn = postgres.connect('dbname = skicasterDB user = postgres password = password')
-            curr = conn.cursor()
+    #Establish connection to database
+    try:
+        conn = postgres.connect('dbname = skicasterDB user = postgres password = password')
+        curr = conn.cursor()
 
-            #Get daily conditions
-            conditions = daily_conditions()
-            push_to_database(conditions, curr, day)
+    except Exception as error:
+        print('The following error occured: %s', error)    
 
-            #Close connection
-            curr.close()
-            conn.close()
+    #Get the daily conditions and push them to database
+    daily_conditions()
 
-        except Exception as error:
-            print('The following error occured: %s', error)    
+    #Close connection
+    curr.close()
+    conn.close()
 
+#Creates a dictionary with none values based off provided keys
+def empty_dictionary(keys):
+    dictionary = {}
+    return dictionary.fromkeys(keys, None)
+        
 
 def daily_conditions():
-        #Download and save today's Avalance Canada data
-        r1 = rq.get('https://avalanche.pc.gc.ca/bulletin-eng.aspx?r=1&d=TODAY')
-        r1.raise_for_status()
-        open('avalanche_data_today.html', 'wb').write(r1.content)
-        open('avalanche_data_today.txt', 'wb').write(r1.content)
-        with open('avalanche_data_today.html', 'r') as file:
-            r1_data = file.read()
 
-        #Parse the html file & use regex to separate the rating from the description
-        r1_soup = soup(r1_data, 'html.parser')
-        data_param = ['#pageContent_lblFirstdayAlpineRateHeader','#pageContent_lblFirstdayTreelineRateHeader', '#pageContent_lblFirstdayBelowtreelineRateHeader' ]
-        result_param = []
-        rating_arr = []
-        rating_desc_arr = []
-        rating_regex = re.compile('\d')
-        rating_desc_regex = re.compile('\w+$')
+    #Initialize dictionary with which we will use to pull data
+    keys = ['AP Rating', 'AP Desc', 'TL Rating', 'TL Desc', 'BT Rating', 'BT Desc']
+    data_dict = empty_dictionary(keys)
 
-        #Run through the loop once for each level (Alpine/Treeline/Below Treeline)
-        #Returns an array eg: [3 ,2, 1, "considerable", "moderate", "low"]
-        for i in range(0,len(data_param)):
-            result = r1_soup.select(str(data_param[i]))
-            result_param.append(result[0].text) 
-            rating_arr.append(int(rating_regex.search(result_param[i]).group()))
-            rating_desc_arr.append(rating_desc_regex.search(result_param[i]).group())
+    #Download and save today's Avalance Canada data
+    r1 = rq.get('https://avalanche.pc.gc.ca/bulletin-eng.aspx?r=1&d=TODAY')
+    r1.raise_for_status()
+    open('avalanche_data_today.html', 'wb').write(r1.content)
+    open('avalanche_data_today.txt', 'wb').write(r1.content)
+    with open('avalanche_data_today.html', 'r') as file:
+        r1_data = file.read()
 
-        
-        return ['Daily'] + rating_arr + rating_desc_arr
+    #Parse the html file & use regex to separate the rating from the description
+    r1_soup = soup(r1_data, 'html.parser')
+    data_param = ['#pageContent_lblFirstdayAlpineRateHeader','#pageContent_lblFirstdayTreelineRateHeader', '#pageContent_lblFirstdayBelowtreelineRateHeader' ]
+    rating_arr = []
+    rating_regex = re.compile('\d')
+    rating_desc_regex = re.compile('\w+$')
 
+    #Run through the loop once for each level (Alpine/Treeline/Below Treeline)
+    #Returns an array eg: [3 ,"considerable", 2, "moderate", 1, "low"]
+    #Puts these values in a dictionary for ease of transfer to db
+    for i in range(0,len(data_param)):
+        result = r1_soup.select(str(data_param[i]))
+        rating_arr.append(int(rating_regex.search(result[0].text).group()))
+        rating_arr.append(rating_desc_regex.search(result[0].text).group())
 
+    i = 0
+    for key in data_dict:
+        data_dict[key] = rating_arr[i]
+        i += 1
 
-def push_to_database(data, curr, date):
-    
-    #Determine the datasource
-    if data[0] in ['Daily']:
+    data_dict['Date'] = str(datetime.date.today())
+    print(data_dict)
 
-        try:
-        pg_insert =  ''' INSERT INTO Location_R1 (BT_Rating, BT_Desc, TL_Rating, TL_Desc, AP_Rating, AP_Desc) VALUES (%s,%s,%s)'''
+    #push results to the database
+    push_to_db(data_dict)
 
-        print('Daily')
-        except:
+    return
 
-
+def push_to_db(dictionary):
+    return
 
 main()
